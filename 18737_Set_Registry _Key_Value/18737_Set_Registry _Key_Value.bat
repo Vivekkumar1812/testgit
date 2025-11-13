21efa58229b038f7d28f5233b7e526916f6f74ea
Vivek <vivek.f.vivek@capgemini.com>
2025-11-13 11:20:34 +0530

    Pussing 18737 story script folder

---
+++ 18737_Set_Registry _Key_Value/18737_Set_Registry _Key_Value.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0\18737_Set_Registry _Key_Value.ps1" %1 %2 %3 %4
--- hello.py
+++
@@ -1,4 +0,0 @@
-# this is a print statement
-print("Bye, World!")
-print("This is from Hello branch")
-print("This is branch 1")
