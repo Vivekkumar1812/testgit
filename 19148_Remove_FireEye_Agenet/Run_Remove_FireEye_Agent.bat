2fa4d6f0cd222f5e441a15fe7dde2e9e76339d86
Vivek <vivek.f.vivek@capgemini.com>
2025-11-21 14:31:04 +0530

    updating batch file with one line launcher

--- 19148_Remove_FireEye_Agenet/Run_Remove_FireEye_Agent.bat
+++ 19148_Remove_FireEye_Agenet/Run_Remove_FireEye_Agent.bat
@@ -1,17 +1,2 @@
-@echo off
-REM Launcher batch file to run the PowerShell FireEye removal script as Administrator.
-REM Usage examples:
-REM   Run_Remove_FireEye_Agent.bat
-REM   Run_Remove_FireEye_Agent.bat "-ProductCode {XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX} -TimeoutSeconds 600"
-
-:: Build PowerShell command line from any passed arguments
-set "ARGS=%~1"
-if "%ARGS%"=="" (
-  set "PSARGS=-ExecutionPolicy Bypass -File "%~dp0\19148_Remove_FireEye_Agent.ps1""
-) else (
-  set "PSARGS=-ExecutionPolicy Bypass -File "%~dp0\19148_Remove_FireEye_Agent.ps1" %ARGS%"
-)
-
-:: Use PowerShell to start elevated if not already elevated. This attempts to use ShellExecute Verb RunAs to prompt for elevation.
-powershell -NoProfile -Command "Start-Process powershell -ArgumentList '%PSARGS%' -Verb RunAs"
-exit /b %ERRORLEVEL%
+@echo off & rem Minimal launcher: elevate and run PS1 in same folder, forwarding all args
+powershell -NoProfile -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0\19148_Remove_FireEye_Agent.ps1\" %*' -Verb RunAs -Wait" & exit /b %ERRORLEVEL%
